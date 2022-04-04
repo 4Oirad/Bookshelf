5808dc8cc1d6b2d2bd9615cefd7093a604dc3ffd
Bair Arshaev <ars-bair@yandex.ru>
2022-04-05 01:06:46 +0300

    Edit code

--- src/Bookshelf.java
+++ src/Bookshelf.java
@@ -2,7 +2,7 @@ public class Bookshelf {
 
     public static void main(String[] args) {
         int[] books = new int[] {14, 16, 19, 32, 32, 32, 56, 69, 72};
-        System.out.println(myFunction(books, 33));
+        System.out.println(myFunction(books, 60));
 
     }
 
@@ -10,46 +10,23 @@ public class Bookshelf {
         int left = 0;
         int right = x.length - 1;
         int index = 0;
-        while (left < right) {
+        while (left <= right) {
             int m = (left + right) / 2;
             if (x[m] > y) {
-                if (x[m-1] == y) {
+                if (m == 0) {
+                    index = -1;
+                    break;
+                }
+                if ((x[m-1] == y) || (x[m-1] < y)) {
                    index = m - 1;
                    break;
                 }
                 right = m - 1;
             } else {
-
                 left = m + 1;
             }
-
         index = m;
         }
         return x.length - 1 - index;
     }
-
-        //f ? System.out.println("нашли") : System.out.println("не нашли");
-        //if (f == true) System.out.println("нашли");
-        //else System.out.println("не нашли");
 }
-
-    /*public static void myFunction(int[] books, int pcs) {
-        int left = 0;
-        int right = books.length - 1;
-        boolean f = false;
-        while (left < right) {
-            int middle = (left + right) / 2;
-            if (books[middle] == pcs) {
-                f = true;
-                break;
-            } else {
-                if (books[middle] > pcs) right = middle - 1;
-                else if (books[middle] < pcs) left = middle + 1;
-            }
-        }
-        if (books[left] == pcs) f = true;
-        //f ? System.out.println("нашли") : System.out.println("не нашли");
-        if (f == true) System.out.println("нашли");
-        else System.out.println("не нашли");
-    }*/
-
