aea1e6d77124d57114e26b0ae46d4ca2883dc52c
Bair Arshaev <ars-bair@yandex.ru>
2022-04-05 16:02:37 +0300

    Edit code by Minya

--- src/Bookshelf.java
+++ src/Bookshelf.java
@@ -2,8 +2,22 @@ public class Bookshelf {
 
     public static void main(String[] args) {
         int[] books = new int[] {14, 16, 19, 32, 32, 32, 56, 69, 72};
-        System.out.println(myFunction(books, 60));
+        System.out.println(muFunctionRev2(books, 73));
 
+    }
+
+    public static int muFunctionRev2(int[] x, int y) {
+        int left = 0;
+        int right = x.length - 1;
+        if (x[right] <= y) return 0;
+        else {
+            while (left < right) {
+                int m = (left + right) / 2;
+                if (x[m] <= y) left = m + 1;
+                else right = m;
+            }
+            return x.length - left;
+        }
     }
 
     public static int myFunction(int[] x, int y) {
